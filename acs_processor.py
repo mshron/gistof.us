1e59ab00010e65d2bd1ecd7c7fc83c2ff749354d
Adam Lerner <eletari@gmail.com>
2010-12-31 01:37:02 -0500

    transform_row & process_acs_file work, no shelf

--- acs_processor.py
+++ acs_processor.py
@@ -1,24 +1,57 @@
-def call_fns_on_data(in_file=None, cols_to_fn_map={}, out_file=None):
+import csv
+
+def addone(n):
+    return {1: int(n[0])+1}
+default_cols_to_fns = {('B01003_1_EST',): addone}
+
+def process_acs_file(acs_file='test_acs.txt', cols_to_fns_map=default_cols_to_fns, shelf='test.shelf'):
 
     #open the file as a CSV with delimiter '|'
+    csvfile = csv.reader(open(acs_file, 'rb'), delimiter='|')    
     
     #store the first row as the column names
-    #store the second row as the column human-readable names
+    machine_names = csvfile.next()
     
+    #store the second row as the column human-readable names
+    human_names = csvfile.next()
+        
     #until the end of the file, for each row...
+    for row in csvfile:
+
         #rewrite the row as a dict (key = column, value = value)
+        row_dict = {}
+        for i, name in enumerate(machine_names):
+            row_dict[name] = row[i]
+                    
         #transform the rowdict, getting back a transformed dict
-        #jsonify, hexify, and write out the transformed dict
+        transformed_row = transform_row(row_dict, cols_to_fns_map)
+        
+        print transformed_row
+        #shelve it
     
 
 def transform_row(row_dict, cols_to_fns_map):
+    row_transformation = {}
+    
+    #for each transformation we need to do per row
     for cols in cols_to_fns_map:     #cols is a tuple of col names
         
-    #for each k/vs of cols_to_fns_map
-        #if all cols are available, call the corresponding fn on the corresponding data and add the returned k/v pair to the output dict
-        #if they are not, error!
+        #check if this is actually a function?
+        fn = cols_to_fns_map[cols]
+                       
+        #check if each column is in the data
+        #if so, add it to cols_values
+        cols_values = []  #cols_values is the arguments to fn
+        for col in cols:
+            if not col in row_dict:
+                raise IndexError('No column %s in the data' % col)
+            else:
+                cols_values.append(row_dict[col])
         
-    #return output dict
+        row_transformation.update(fn(cols_values))
+    
+    #return the row transformation
+    return row_transformation
     
     
 #transforming rows
@@ -31,4 +64,17 @@ def transform_row(row_dict, cols_to_fns_map):
 #map fns
 #take in a tuple of values, output key/value pair
 #key is ultimate JSON name of doom, value is uJSON value od
+
+
+
+def addtwo(n):
+    return {2: n[0]+2}
+    
+def sum(n):
+    return {'s': n[0]+n[1]}
+
+cols_to_fns_map = {('first',): addone, 
+                   ('second',): addtwo,
+                   ('first','second'): sum}
+row_dict = {'first': 10, 'second': 20, 'third': 50}
 
