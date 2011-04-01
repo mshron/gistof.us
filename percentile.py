8947090a56e7022e1d7450cee78672aa1a58d890
Adam Lerner <eletari@gmail.com>
2011-04-01 14:15:13 -0700

    Added __name__/__main__ encapsule to percentile.py

--- percentile.py
+++ percentile.py
@@ -28,6 +28,10 @@ def percentile(l, n, ile=5, presort=True):
             return i-1
     return ile-1                    #must be top percentile if we get here
 
+        
+def histogram(l, bins=10):
+    pass   
+     
 
 def setup_shelf(shelf_file):
     shelf = shelve.open(shelf_file)
@@ -40,50 +44,54 @@ scalar_targets = [
     ('hispanic_or_latino', 'pct_hispanic_or_latino')
 ]
 
-usage = "usage: %prog [options] shelf_1 [shelf_2 ... shelf_n] -o result.json\nFirst shelf is primary; its keys are the final ones."
-parser = OptionParser(usage=usage)
-parser.add_option("-o", "--out", dest="out_filename", 
-            help="output percentile data to shelf named FILE", metavar="FILE")
+def main():
+    usage = "usage: %prog [options] shelf_1 [shelf_2 ... shelf_n] -o result.json\nFirst shelf is primary; its keys are the final ones."
+    parser = OptionParser(usage=usage)
+    parser.add_option("-o", "--out", dest="out_filename", 
+                help="output percentile data to shelf named FILE", metavar="FILE")
 
-(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-#open in-shelves
-in_shelves = [shelve.open(sh,'r') for sh in args]
-out = {}
+    #open in-shelves
+    in_shelves = [shelve.open(sh,'r') for sh in args]
+    out = {}
 
-sys.stderr.write("Opened in-files for reading.\n")
+    sys.stderr.write("Opened in-files for reading.\n")
 
-#open out-shelf
-out_shelf = setup_shelf(options.out_filename)
-sys.stderr.write("Opened out-file for writing.\n")
+    #open out-shelf
+    out_shelf = setup_shelf(options.out_filename)
+    sys.stderr.write("Opened out-file for writing.\n")
 
-#amal: {tractid => value} for the current target stat
-amal = {}
-for t in scalar_targets:
-    m = t[0] #main stat category
-    s = t[1] #subcategory
-    print("%s > %s" % (m, s))
-    for in_shelf in in_shelves:
-        for tractid in in_shelf:
-            #skip the tid for this shelf if this shelf doesn't have
-            #the stat we're looking for
-            if in_shelf[tractid].get(m,{}).get(s,{}) != {}: 
-                #don't overwrite a value from a prior shelf
-                #the earlier ones have precedence
-                if not amal.has_key(tractid):
-                    amal[tractid] = in_shelf[tractid][m][s]
-
-    obs_list = sorted([amal[tid] for tid in amal])
-    for i,tractid in enumerate(amal):
-        ile = percentile(obs_list, amal[tractid], ile=100)
-        tract = out_shelf.get(tractid, {})
-        tract.setdefault(m, {})[s+'_percentile'] = ile
-        out_shelf[tractid] = tract
-        if (i!= 0) and ((i%100) == 0):
-            out_shelf.sync()
-    
+    #amal: {tractid => value} for the current target stat
     amal = {}
+    for t in scalar_targets:
+        m = t[0] #main stat category
+        s = t[1] #subcategory
+        print("%s > %s" % (m, s))
+        for in_shelf in in_shelves:
+            for tractid in in_shelf:
+                #skip the tid for this shelf if this shelf doesn't have
+                #the stat we're looking for
+                if in_shelf[tractid].get(m,{}).get(s,{}) != {}: 
+                    #don't overwrite a value from a prior shelf
+                    #the earlier ones have precedence
+                    if not amal.has_key(tractid):
+                        amal[tractid] = in_shelf[tractid][m][s]
+
+        obs_list = sorted([amal[tid] for tid in amal])
+        for i,tractid in enumerate(amal):
+            ile = percentile(obs_list, amal[tractid], ile=100)
+            tract = out_shelf.get(tractid, {})
+            tract.setdefault(m, {})[s+'_percentile'] = ile
+            out_shelf[tractid] = tract
+            if (i!= 0) and ((i%100) == 0):
+                out_shelf.sync()
+        
+        amal = {}
 
 
-out_shelf.close()
-sys.stderr.write('Done\n')
+    out_shelf.close()
+    sys.stderr.write('Done\n')
+
+if __name__ == '__main__':
+    main()
