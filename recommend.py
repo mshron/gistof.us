4fd46697c33d9d86ee939a8927e52cd2dba26796
Adam Lerner <eletari@gmail.com>
2011-04-22 13:55:10 -0700

    Recommend gives correct categories and stat names
    Previously it was giving the stat name and percentile
    stat name as category/name, now it gives category/name

--- recommend.py
+++ recommend.py
@@ -6,46 +6,50 @@ def reach(data, cat, vs, ps):
 
 def prepare((s, values)):
     out =   {'sentence': values[0], 
-            'category': values[1],
-            'name': values[2]}
+            'name': values[1],
+            'category': values[3],
+    }
     return out
 
 
 def poverty(data):
+    cat = 'poverty'
     vs = 'pct_below_100pc'
     ps = 'pct_below_100pc_percentile'
-    v, p = reach(data, 'poverty', vs, ps)
+    v, p = reach(data, cat, vs, ps)
     if p > 80:
         ss = '...many people are income-*poor* (%.02f%%)'%float(100*v)
     elif p < 20:
         ss = '...*few* people are income-poor (%.02f%%)'%float(100*v)
     else:
         ss = '...people are not unusually poor (%.02f%%)'%float(100*v)
-    return (v,(ss, vs, ps))
+    return (v,(ss, vs, ps, cat))
 
 def hispanic(data):
+    cat = 'hispanic_or_latino'
     vs = 'pct_hispanic_or_latino'
     ps = 'pct_hispanic_or_latino_percentile'
-    v, p = reach(data, 'hispanic_or_latino', vs, ps)
+    v, p = reach(data, cat, vs, ps)
     if p > 80:
         ss = '...a large portion of the population is hispanic/latino (%.02f%%)'%float(100*v)
     elif p < 20:
         ss = '...a relatively *small portion* of the population is hispanic/latino (%.02f%%)'%float(100*v)
     else:
         ss = '...the population is not particularly hispanic/latino (%.02f%%)'%float(100*v)
-    return (v,(ss, vs, ps))
+    return (v,(ss, vs, ps, cat))
 
 def veteran(data):
+    cat = 'veteran_status'
     vs = 'pct_veteran'
     ps = 'pct_veteran_percentile'
-    v, p = reach(data, 'veteran_status', vs, ps)
+    v, p = reach(data, cat, vs, ps)
     if p > 80:
         ss = "...many people are *veterans* (%.02f%%)"%float(100*v)
     if p < 20:
         ss = "...few people are *veterans* (%.02f%%)"%float(100*v)
     else:
         ss = "...there are not an especially high concentration of veterans (%.02f%%)"%float(100*v)
-    return (v,(ss, vs, ps))
+    return (v,(ss, vs, ps, cat))
 
 
 transforms = [poverty, hispanic, veteran]
